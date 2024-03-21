159bbf1ab34dd71660826b745891ecfad9fb2c45
Yusif Yusifli <yusifry@code.edu.az>
2024-03-22 02:59:49 +0400

    first version

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,19 @@
+﻿namespace ConsoleApp1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("balinizi daxil edin:");
+            int sonuc = int.Parse(Console.ReadLine());
+
+            if(sonuc > 50 )
+            {
+                Console.WriteLine("ugurlu");
+            }
+            else { Console.WriteLine("ugursuz"); }
+
+
+        }
+    }
+}
